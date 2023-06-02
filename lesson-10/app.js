d5983610d3ddc8c936e617b017775e696c4fa912
TaN Taa <atan60896@gmail.com>
2023-06-02 22:45:32 +0500

    New Update Lesson-9 and Lesson-10

---
+++ lesson-10/app.js
@@ -0,0 +1,175 @@
+
+
+// 1-masala
+
+// let a = +prompt("Son kiriting");
+
+
+// if (a>=0){
+//     alert(true)
+// }else{
+//     alert(false)
+// }
+
+
+// 2-masala
+
+// let a = +prompt("Son kiriting");
+
+
+// if (a % 2 === 0){
+//     alert("Juft")
+// }else{
+//     alert("Toq")
+// }
+
+
+// 3-masala
+
+// let a = +prompt("A Sonni kiriting");
+// let b = +prompt("B Sonni kiriting");
+
+// if (a > 0 && b > 0){
+//     alert(true)
+// }else{
+//     alert(false)
+// }
+
+// 4-masala
+
+// let a = +prompt("A Sonni kiriting");
+// let b = +prompt("B Sonni kiriting");
+// let c = +prompt("C Sonni kiriting");
+
+
+// if (a > b && b > c){
+//     alert(true)
+// }else{
+//     alert(false)
+// }
+
+
+// 5-masala
+
+// let a = +prompt("A Sonni kiriting");
+// let b = +prompt("B Sonni kiriting");
+// let c = +prompt("C Sonni kiriting");
+
+
+// if ((a > b && b > c) || (a < b && b < c)){
+//     alert("B son A va C orasida yotadi ")
+// }else{
+//     alert("B son A va C orasida yotmaydi")
+// }
+
+
+// 6-masala
+
+// let a = +prompt("A Sonni kiriting");
+// let b = +prompt("B Sonni kiriting");
+
+// if (a % 2 == 0 && b % 2 == 0){
+//     alert("A ham B ham juft");
+// }else if(a % 2 == 0 && b % 2 == 1){
+//     alert("A juft B toq");
+// }else if(a % 2 == 1 && b % 2 == 0){
+//     alert("A toq B juft");
+// }else{
+//     alert("A ham B ham toq");
+// }
+
+// 8-masala
+
+// let a = +prompt("A Sonni kiriting");
+// let b = +prompt("B Sonni kiriting");
+// let c = +prompt("C Sonni kiriting");
+// let d = +prompt("D Sonni kiriting");
+
+// if (a >= 0 && b >= 0 && c >= 0 && d >= 0){
+//     alert(true)
+// }else(
+//     alert(false)
+// )
+
+
+// 10-masala
+
+// let a = +prompt("Hafta kuni");
+
+
+// if (a === 1){
+//     alert("Monday")
+// }else if(a === 2){
+//     alert("Tuesday")
+// }else if(a === 3){
+//     alert("Wensday")
+// }else if(a === 4){
+//     alert("Thursday")
+// }else if(a === 5){
+//     alert("Friday")
+// }else if(a === 6){
+//     alert("Saturday")
+// }else if(a === 7){
+//     alert("Sunday")
+// }else{
+//     alert("Not found")
+// }
+
+// 11-masala
+
+// let a = +prompt("Telefon kodini kiriting");
+
+// if(a === 91 || a === 90 ){
+//     alert("Beeline")
+// }else if(a === 99){
+//     alert("uzTelecom")
+// }else if(a === 93 || a === 94){
+//     alert("ucell")
+// }
+
+// 12-13-masala
+
+// let a = +prompt("Son kiriting");
+
+// if(a % 2 === 0 ){
+//     alert(a + 1)
+// }else{
+//     alert(a - 1)
+// }
+
+
+// 14-masala
+
+// let a = +prompt("A ni kiriting");
+// let b = +prompt("B ni kiriting");
+
+// if(a > b){
+//     alert(a)
+// }else{
+//     alert(b)
+// }
+
+// 15-16-masala
+
+// let a = +prompt("A ni kiriting");
+// let b = +prompt("B ni kiriting");
+// let c = +prompt("C ni kiriting");
+// let group = [a, b, c];
+
+// let max = group[0];
+// let min = group[0];
+// for(let i = 0; i < group.length; i++){
+//     if (group[i] >= max){
+//         max = group[i];
+//     }
+//     if (group[i] <= min){
+//         min = group[i];
+//     }
+// }
+
+// alert(max);
+// alert(min);
+
+
+// 17-masala
+
